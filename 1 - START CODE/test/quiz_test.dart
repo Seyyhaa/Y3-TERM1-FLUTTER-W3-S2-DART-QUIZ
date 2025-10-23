a30616540d165a4b064591434efa1aa55f1fd1ee
Seyha Kruy <seyhakruy168168@gmail.com>
2025-10-23 22:58:43 +0700

    Update Test

--- 1 - START CODE/test/quiz_test.dart
+++ 1 - START CODE/test/quiz_test.dart
@@ -4,35 +4,35 @@ import 'package:test/test.dart';
 
 void main() {
   Question q1 =
-      Question(title: "4-2", choices: ["1", "2", "3"], goodChoice: "2",point:100);
+      Question(title: "4-2", choices: ["1", "2", "3"], goodChoice: "2",point:50);
   Question q2 =
-      Question(title: "4+2", choices: ["1", "2", "3"], goodChoice: "6",point:102);
+      Question(title: "4+2", choices: ["1", "2", "3"], goodChoice: "6",point:50);
 
   Quiz quiz = Quiz(questions: [q1, q2]);
 
   setUp(() {});
 
   test("test 1", () {
-    List<Answer> answers = [];
-    answers.add(Answer(question: q1, answerChoice: "2"));
-    answers.add(Answer(question: q2, answerChoice: "6"));
-    quiz.answers = answers;
-
+    Player seyha = quiz.getOrCreatePlayer("Seyha");
+    seyha.addAnswer(Answer(question: q1, answerChoice: "2"));
+    seyha.addAnswer(Answer(question: q2, answerChoice: "6"));
+    
+    Map<String, int> result = quiz.getScore(seyha);
     // Score should be 100%
-    expect(quiz.getScore()['Score'], equals(100));
-    expect(quiz.getScore()['Percentage'], equals(100));
+    expect(result['Score'], equals(100));
+    expect(result['Percentage'], equals(100));
   });
 
   test("test 2", () {
-    List<Answer> answers = [];
-    answers.add(Answer(question: q1, answerChoice: "7"));
-    answers.add(Answer(question: q2, answerChoice: "9"));
-    quiz.answers = answers;
+    Player sok = quiz.getOrCreatePlayer("Sok");
+    sok.addAnswer(Answer(question: q1, answerChoice: "7"));
+    sok.addAnswer(Answer(question: q2, answerChoice: "9"));
+    
+  Map<String, int> result = quiz.getScore(sok);
+    
 
-    print("asnwer size = ${quiz.answers.length}");
-
-    // Score should be 100%
-    expect(quiz.getScore()['Score'], equals(0));
-    expect(quiz.getScore()['Percentage'], equals(0));
+    // Score should be 0%
+    expect(result['Score'], equals(0));
+    expect(result['Percentage'], equals(0));
   });
 }
